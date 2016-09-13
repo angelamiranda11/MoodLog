f5d3483af7a8bb05a3ca0c53fecc91b92fb0a815
dreigamboa <dreigamboa@gmail.com>
2016-09-13 10:39:14 +0800

    Added some words in the bow-algorithm-reference.txt --d

--- POC_MoodLog/POC_MoodLog/Program.cs
+++ POC_MoodLog/POC_MoodLog/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,75 @@ namespace POC_MoodLog
 {
     class Program
     {
+        static String[] bowreference;
+        static String[] emoticons;
+        static String[] punctuations;
         static void Main(string[] args)
         {
+            String input = Console.ReadLine();
+
+            bowreference = POC_MoodLog.Properties.Resources.bow_algorithm_reference.Split();
+            emoticons = POC_MoodLog.Properties.Resources.emoticons.Split();
+            punctuations = POC_MoodLog.Properties.Resources.punctuations.Split();
+
+
+
+            Console.ReadKey();
+        }
+
+        public static IEnumerable<string> makeBigrams(string text)
+        {
+            int nGramSize = 2;
+            StringBuilder nGram = new StringBuilder();
+            Queue<int> wordLengths = new Queue<int>();
+
+            int wordCount = 0;
+            int lastWordLen = 0;
+
+            if (text != "" && char.IsLetterOrDigit(text[0]))
+            {
+                nGram.Append(text[0]);
+                lastWordLen++;
+            }
+
+            //generate ngrams
+            for (int i = 1; i < text.Length - 1; i++)
+            {
+                char before = text[i - 1];
+                char after = text[i + 1];
+
+                if (char.IsLetterOrDigit(text[i])
+                        ||
+                        (text[i] != ' '
+                        && (char.IsSeparator(text[i]) || char.IsPunctuation(text[i]))
+                        && (char.IsLetterOrDigit(before) && char.IsLetterOrDigit(after))
+                        )
+                    )
+                {
+                    nGram.Append(text[i]);
+                    lastWordLen++;
+                }
+                else
+                {
+                    if (lastWordLen > 0)
+                    {
+                        wordLengths.Enqueue(lastWordLen);
+                        lastWordLen = 0;
+                        wordCount++;
+
+                        if (wordCount >= nGramSize)
+                        {
+                            yield return nGram.ToString();
+                            nGram.Remove(0, wordLengths.Dequeue() + 1);
+                            wordCount -= 1;
+                        }
+
+                        nGram.Append(" ");
+                    }
+                }
+            }
+            nGram.Append(text.Last());
+            yield return nGram.ToString();
         }
     }
 }
