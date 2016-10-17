b9c88914981381622c4716c5a7cfce2d9b33c071
Christine <2013056563@ust-ics.mygbiz.com>
2016-10-17 21:22:08 +0800

    Some code cleanup

--- wpf-moodlog/wpf-moodlog/MoodLogEntriesPage.xaml.cs
+++ wpf-moodlog/wpf-moodlog/MoodLogEntriesPage.xaml.cs
@@ -30,14 +30,95 @@ namespace wpf_moodlog
             // loadPreviousEntries();
         }
 
-        private void setDateTodayLabel()
+        private void joyButton_Click(object sender, RoutedEventArgs e)
         {
-            // Get the current date.
-            DateTime thisDay = DateTime.Today;
+            enableAllMainEmoticonButtonsExcept(joyButton);
 
-            dateTodayLabel.Content = thisDay.ToString("MMM d");
+            showSubEmoticons(new string[] {
+                "EmoticonJoy",
+                "EmoticonJoy",
+                "EmoticonJoy",
+                "EmoticonJoy",
+                "EmoticonJoy",
+                "EmoticonJoy",
+                "EmoticonJoy",
+            });
         }
 
+        private void sadnessButton_Click(object sender, RoutedEventArgs e)
+        {
+            enableAllMainEmoticonButtonsExcept(sadnessButton);
+
+            showSubEmoticons(new string[] {
+                "EmoticonSadness",
+                "EmoticonSadness",
+                "EmoticonSadness",
+                "EmoticonSadness",
+                "EmoticonSadness",
+                "EmoticonSadness",
+                "EmoticonSadness",
+            });
+        }
+
+        private void disgustButton_Click(object sender, RoutedEventArgs e)
+        {
+            enableAllMainEmoticonButtonsExcept(disgustButton);
+
+            showSubEmoticons(new string[] {
+                "EmoticonDisgust",
+                "EmoticonDisgust",
+                "EmoticonDisgust",
+                "EmoticonDisgust",
+                "EmoticonDisgust",
+                "EmoticonDisgust",
+                "EmoticonDisgust",
+            });
+        }
+
+        private void angerButton_Click(object sender, RoutedEventArgs e)
+        {
+            enableAllMainEmoticonButtonsExcept(angerButton);
+
+            showSubEmoticons(new string[] {
+                "EmoticonAnger",
+                "EmoticonAnger",
+                "EmoticonAnger",
+                "EmoticonAnger",
+                "EmoticonAnger",
+                "EmoticonAnger",
+                "EmoticonAnger",
+            });
+        }
+
+        private void surpriseButton_Click(object sender, RoutedEventArgs e)
+        {
+            enableAllMainEmoticonButtonsExcept(surpriseButton);
+
+            showSubEmoticons(new string[] {
+                "EmoticonSurprised",
+                "EmoticonSurprised",
+                "EmoticonSurprised",
+                "EmoticonSurprised",
+                "EmoticonSurprised",
+                "EmoticonSurprised",
+                "EmoticonSurprised",
+            });
+        }
+
+        private void fearButton_Click(object sender, RoutedEventArgs e)
+        {
+            enableAllMainEmoticonButtonsExcept(fearButton);
+
+            showSubEmoticons(new string[] {
+                "EmoticonFear",
+                "EmoticonFear",
+                "EmoticonFear",
+                "EmoticonFear",
+                "EmoticonFear",
+                "EmoticonFear",
+                "EmoticonFear",
+            });
+        }
 
         private void profileButton_Click(object sender, RoutedEventArgs e)
         {
@@ -46,27 +127,36 @@ namespace wpf_moodlog
             this.NavigationService.Navigate(moodLogProfilePage);
         }
 
-        private void addToEntryTextBox(String str)
+        private void hashtagButton_Click(object sender, RoutedEventArgs e)
+        {
+            appendToEntryTextBox("#");
+        }
+
+        private void subEmoticonButton_Click(object sender, RoutedEventArgs e)
+        {
+            Button subEmoticonButton = e.Source as Button;
+            appendToEntryTextBox(":" + subEmoticonButton.Name + ":");
+        }
+
+        private void setDateTodayLabel()
+        {
+            // Get the current date.
+            DateTime thisDay = DateTime.Today;
+
+            dateTodayLabel.Content = thisDay.ToString("MMM d");
+        }
+
+        private void appendToEntryTextBox(String str)
         {
             entryTextBox.Text += str;
             entryTextBox.Focus();
             entryTextBox.Select(entryTextBox.Text.Length, 0);
         }
 
-        private void hashtagButton_Click(object sender, RoutedEventArgs e)
-        {
-            addToEntryTextBox("#");
-        }
 
-        private void disableAllEmoticonButtonsExcept(Button emoticonButton)
+        private void enableAllMainEmoticonButtonsExcept(Button mainEmoticonButtonToDisable)
         {
-            enableAllEmoticonButtons();
-            emoticonButton.Background = Brushes.DarkGray;
-        }
-
-        private void enableAllEmoticonButtons()
-        {
-            Button[] emoticonButtons =
+            Button[] mainEmoticonButtons =
             {
                 joyButton,
                 sadnessButton,
@@ -76,38 +166,57 @@ namespace wpf_moodlog
                 fearButton
             };
 
-            foreach(Button emoticonButton in emoticonButtons)
+            foreach(Button mainEmoticonButton in mainEmoticonButtons)
             {
-                emoticonButton.Background = null;
+                enableEmoticonButton(mainEmoticonButton);
             }
+
+            disableEmoticonButton(mainEmoticonButtonToDisable);
         }
 
+        private void enableEmoticonButton(Button emoticonButton)
+        {
+            emoticonButton.Background = null;
+        }
 
-        private void showSubEmoticonsPanel(String[] subEmoticonNames)
+        private void disableEmoticonButton(Button emoticonButton)
+        {
+            emoticonButton.Background = Brushes.DarkGray;
+        }
+
+        private void showSubEmoticons(String[] subEmoticonNames)
         {
             resetSubEmoticonsPanel();
 
             foreach (String subEmoticonName in subEmoticonNames)
             {
-                Button subEmoticonButton = new Button();
-   
-                subEmoticonButton.Content = convertUriToImage(new Uri("Images/" + subEmoticonName + ".png", UriKind.Relative));
-                subEmoticonButton.Width = 50;
-                subEmoticonButton.Height = 40;
-                subEmoticonButton.Background = null;
-                subEmoticonButton.BorderThickness = new Thickness(0, 0, 1, 0);
-                subEmoticonButton.ToolTip = subEmoticonName.Replace('-', ' ');
-                subEmoticonButton.Name = subEmoticonName;
-                subEmoticonButton.Click += new RoutedEventHandler(subEmoticonButton_Click);
+                Button subEmoticonButton = createSubEmoticonButtonWithName(subEmoticonName);
 
                 subEmoticonsPanel.Children.Add(subEmoticonButton);
             }
         }
 
-        private void subEmoticonButton_Click(object sender, RoutedEventArgs e)
+        private Button createSubEmoticonButtonWithName(String subEmoticonName)
         {
-            Button subEmoticonButton = e.Source as Button;
-            addToEntryTextBox(":" + subEmoticonButton.Name + ":");
+            Button newSubEmoticonButton = new Button();
+
+            setPropertiesOfSubEmoticonButton(newSubEmoticonButton, subEmoticonName);
+
+            return newSubEmoticonButton;
+        }
+
+        private Button setPropertiesOfSubEmoticonButton(Button subEmoticonButton, String subEmoticonName)
+        {
+            subEmoticonButton.Content = convertUriToImage(new Uri("Images/" + subEmoticonName + ".png", UriKind.Relative));
+            subEmoticonButton.Width = 50;
+            subEmoticonButton.Height = 40;
+            subEmoticonButton.Background = null;
+            subEmoticonButton.BorderThickness = new Thickness(0, 0, 1, 0);
+            subEmoticonButton.ToolTip = subEmoticonName.Replace('-', ' ');
+            subEmoticonButton.Name = subEmoticonName;
+            subEmoticonButton.Click += new RoutedEventHandler(subEmoticonButton_Click);
+
+            return subEmoticonButton;
         }
 
         private Image convertUriToImage(Uri uri)
@@ -125,106 +234,13 @@ namespace wpf_moodlog
             subEmoticonsPanel.Background = Brushes.DarkGray;
         }
 
-        private void joyButton_Click(object sender, RoutedEventArgs e)
+        private Brush convertHexToBrush(String hex)
         {
-            disableAllEmoticonButtonsExcept(joyButton);
+            BrushConverter brushConverter = new BrushConverter();
+            Brush brush = (Brush)brushConverter.ConvertFrom(hex);
+            brush.Freeze();
 
-            String[] joyEmoticonNames = {
-                "EmoticonJoy",
-                "EmoticonJoy",
-                "EmoticonJoy",
-                "EmoticonJoy",
-                "EmoticonJoy",
-                "EmoticonJoy",
-                "EmoticonJoy",
-            };
-
-            showSubEmoticonsPanel(joyEmoticonNames);
-        }
-
-        private void sadnessButton_Click(object sender, RoutedEventArgs e)
-        {
-            disableAllEmoticonButtonsExcept(sadnessButton);
-
-            String[] sadnessEmoticonNames = {
-                "EmoticonSadness",
-                "EmoticonSadness",
-                "EmoticonSadness",
-                "EmoticonSadness",
-                "EmoticonSadness",
-                "EmoticonSadness",
-                "EmoticonSadness",
-            };
-
-            showSubEmoticonsPanel(sadnessEmoticonNames);
-        }
-
-        private void disgustButton_Click(object sender, RoutedEventArgs e)
-        {
-            disableAllEmoticonButtonsExcept(disgustButton);
-
-            String[] disgustEmoticonNames = {
-                "EmoticonDisgust",
-                "EmoticonDisgust",
-                "EmoticonDisgust",
-                "EmoticonDisgust",
-                "EmoticonDisgust",
-                "EmoticonDisgust",
-                "EmoticonDisgust",
-            };
-
-            showSubEmoticonsPanel(disgustEmoticonNames);
-        }
-
-        private void angerButton_Click(object sender, RoutedEventArgs e)
-        {
-            disableAllEmoticonButtonsExcept(angerButton);
-
-            String[] angerEmoticonNames = {
-                "EmoticonAnger",
-                "EmoticonAnger",
-                "EmoticonAnger",
-                "EmoticonAnger",
-                "EmoticonAnger",
-                "EmoticonAnger",
-                "EmoticonAnger",
-            };
-
-            showSubEmoticonsPanel(angerEmoticonNames);
-        }
-
-        private void surpriseButton_Click(object sender, RoutedEventArgs e)
-        {
-            disableAllEmoticonButtonsExcept(surpriseButton);
-
-            String[] surprisedEmoticonNames = {
-                "EmoticonSurprised",
-                "EmoticonSurprised",
-                "EmoticonSurprised",
-                "EmoticonSurprised",
-                "EmoticonSurprised",
-                "EmoticonSurprised",
-                "EmoticonSurprised",
-            };
-
-            showSubEmoticonsPanel(surprisedEmoticonNames);
-        }
-
-        private void fearButton_Click(object sender, RoutedEventArgs e)
-        {
-            disableAllEmoticonButtonsExcept(fearButton);
-
-            String[] fearEmoticonNames = {
-                "EmoticonFear",
-                "EmoticonFear",
-                "EmoticonFear",
-                "EmoticonFear",
-                "EmoticonFear",
-                "EmoticonFear",
-                "EmoticonFear",
-            };
-
-            showSubEmoticonsPanel(fearEmoticonNames);
+            return brush;
         }
 
         private void addEntryButton_Click(object sender, RoutedEventArgs e)
@@ -241,18 +257,5 @@ namespace wpf_moodlog
             entriesStackPanel.Children.Add(newEntry);
         }
 
-        private Brush convertHexToBrush(String hex)
-        {
-            BrushConverter brushConverter = new BrushConverter();
-            Brush brush = (Brush)brushConverter.ConvertFrom(hex);
-            brush.Freeze();
-
-            return brush;
-        }
-
-        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
-        }
     }
 }
