ec093c53886a673f4b3b6480f7f1629b2cab93de
Christine <2013056563@ust-ics.mygbiz.com>
2016-10-23 22:29:56 +0800

    Fixed layout of profile page

--- wpf-moodlog/wpf-moodlog/MoodLogProfilePage.xaml.cs
+++ wpf-moodlog/wpf-moodlog/MoodLogProfilePage.xaml.cs
@@ -35,11 +35,69 @@ namespace wpf_moodlog
             dateTodayLabel.Content = thisDay.ToString("MMM d");
         }
 
-        private void entriesButton_Click(object sender, RoutedEventArgs e)
+        private void profileButton_Click(object sender, RoutedEventArgs e)
         {
-            // View Entries page
-            MoodLogEntriesPage moodLogEntriesPage = new MoodLogEntriesPage();
-            this.NavigationService.Navigate(moodLogEntriesPage);
+            // View Profile page
+            MoodLogProfilePage moodLogProfilePage = new MoodLogProfilePage();
+            this.NavigationService.Navigate(moodLogProfilePage);
+        }
+
+        private void profileButton_MouseEnter(object sender, RoutedEventArgs e)
+        {
+            profileLabel.Foreground = Brushes.White;
+            profileImage.Source = new BitmapImage(new Uri("Images/profile-white.png", UriKind.Relative));
+        }
+
+        private void profileButton_MouseLeave(object sender, RoutedEventArgs e)
+        {
+            profileLabel.Foreground = App.Current.Resources["DarkPurple"] as SolidColorBrush;
+            profileImage.Source = new BitmapImage(new Uri("Images/profile-black.png", UriKind.Relative));
+        }
+
+        private void entriesButton_MouseEnter(object sender, RoutedEventArgs e)
+        {
+            entriesLabel.Foreground = Brushes.White;
+            entriesImage.Source = new BitmapImage(new Uri("Images/entries-white.png", UriKind.Relative));
+        }
+
+        private void entriesButton_MouseLeave(object sender, RoutedEventArgs e)
+        {
+            entriesLabel.Foreground = App.Current.Resources["DarkViolet"] as SolidColorBrush;
+            entriesImage.Source = new BitmapImage(new Uri("Images/entries-black.png", UriKind.Relative));
+        }
+
+        private void statsButton_Click(object sender, RoutedEventArgs e)
+        {
+            // View Stats page
+        }
+
+        private void statsButton_MouseEnter(object sender, RoutedEventArgs e)
+        {
+            statsLabel.Foreground = Brushes.White;
+            statsImage.Source = new BitmapImage(new Uri("Images/stats-white.png", UriKind.Relative));
+        }
+
+        private void statsButton_MouseLeave(object sender, RoutedEventArgs e)
+        {
+            statsLabel.Foreground = App.Current.Resources["LightPurple"] as SolidColorBrush;
+            statsImage.Source = new BitmapImage(new Uri("Images/stats-black.png", UriKind.Relative));
+        }
+
+        private void logoutButton_Click(object sender, RoutedEventArgs e)
+        {
+            // View Login page
+        }
+
+        private void logoutButton_MouseEnter(object sender, RoutedEventArgs e)
+        {
+            logoutLabel.Foreground = Brushes.White;
+            logoutImage.Source = new BitmapImage(new Uri("Images/logout-white.png", UriKind.Relative));
+        }
+
+        private void logoutButton_MouseLeave(object sender, RoutedEventArgs e)
+        {
+            logoutLabel.Foreground = App.Current.Resources["Green"] as SolidColorBrush;
+            logoutImage.Source = new BitmapImage(new Uri("Images/logout-black.png", UriKind.Relative));
         }
     }
 }
